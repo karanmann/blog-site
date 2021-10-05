7c6c2bc938114b1d3ac36fa7b03a65230786439a
Karan Mann <62376840+karanmann@users.noreply.github.com>
2021-10-05 14:47:36 +0200

    Base Project - Fully funtional Done

--- scripts/cardsFetch.js
+++ scripts/cardsFetch.js
@@ -2,24 +2,21 @@ const popularDishes = document.getElementById("popular-dishes")
 const todaysSpecial = document.getElementById("todays-special")
 const customerReviews = document.getElementById("customer-reviews")
 
-const fetchCardsData = () => {
-  fetch ('../data/localData.json')
-    .then( res => res.json())
-    .then( json => {
-      buildPopularDishesCards(json);
-      buildTodaysSpecialCards(json);
-      buildReviewCards(json);
-    })
-    .catch(error => console.log(error, "There has been an error"))
-}
+// const randomPriceGenerator = Math.floor(Math.random() * 200) 
+//Generates a random number betweeon 0 & 200
+// substitute the line with <p>Kr. ${dishData.price}</p> to get data from fetch
 
 const buildPopularDishesCards = (json) => {
-  let output = "";
+  let output = ""
   json.popular_dishes.map((dishData) => {
+    console.log(dishData);
     output += `
         <figure class="popular-card">
-          <img class="popular-card-image" src=${dishData.image_url} alt="Dish Image"/>
-          <div class="add-to-fav"> <i class="ion-android-add"></i><span>Add to Favourite</span></div>
+          <img class="popular-card-image"src=${dishData.image_url} alt="Dish Image"/>
+          <div class="add-to-fav"> 
+            <i class="ion-android-add"></i>
+            <span>Add to Favourite</span>
+          </div>
           <figcaption>
             <h2>${dishData.name}</h2>
             <p>${dishData.star_rating}</p>
@@ -31,11 +28,11 @@ const buildPopularDishesCards = (json) => {
         </figure>   
               `;
     popularDishes.innerHTML = output;
-  });
+  })
 }
 
 const buildTodaysSpecialCards = (json) => {
-  let output = "";
+  let output = ""
   json.todays_special.map((dishData) => {
     output += `
         <div class="card-todays-special">
@@ -47,37 +44,52 @@ const buildTodaysSpecialCards = (json) => {
               <img src="./assets/icons/heart.svg" alt="heart-icon">
             </div>
           </div>
-          <h5>${dishData.name}</h5>
+          <h4>${dishData.name}</h4>
           <p>${dishData.short_description}</p>
           <div class="card-bottom-container">
             <h3>Kr. ${dishData.price}</h3>
             <button>ADD TO CART</button>
           </div>
-        </div>`;
+        </div>`
     todaysSpecial.innerHTML = output;
-  });
+  })
 }
 
 const buildReviewCards = (json) => {
-  let output = "";
+  let output = ""
   json.customer_reviews.map( reviewData => {
     output += `
         <div class="review-card">
           <div class="top-container">
             <img class="avatar-image" src=${reviewData.avatar_image_url} alt="Image">
             <div class="review-card-top-center-div">
-              <h5 class="avatar-text">${reviewData.name}</h5>
-              <p>${reviewData.location}</p>
               <div class="star-container">
+                <h5 class="avatar-text">${reviewData.name}</h5>
+                <p>${reviewData.location}</p>
                 <p>${reviewData.star_rating}</p>
               </div>
             </div>
             <img class="quotes-image" src=${reviewData.quote_image} alt="closing-quotes-image">
           </div>
           <p>${reviewData.review}</p>
+          
         </div>`;
     customerReviews.innerHTML = output;
   })
 }
+
+const fetchCardsData = () => {
+  fetch ('../data/localData.json')
+    .then( res => res.json())
+    .then( json => {
+
+      buildPopularDishesCards(json);
+      buildTodaysSpecialCards(json);
+      buildReviewCards(json);
+
+    })
+    .catch(error => console.log(error, "There has been an error"))
+}
+
 
 fetchCardsData()
